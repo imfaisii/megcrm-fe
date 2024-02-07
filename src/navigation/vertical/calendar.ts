f7df94c9e3890345df37d230e302ff8e76076b95
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2024-02-07 17:18:51 +0000

    Update calendar.ts

--- src/navigation/vertical/calendar.ts
+++ src/navigation/vertical/calendar.ts
@@ -3,10 +3,12 @@ import type { VerticalNavItems } from '@/@layouts/types'
 export default [
   {
     heading: 'Events',
+    permissions: ['auth.*']
   },
   {
     title: 'Calendar',
     icon: { icon: 'mdi-calendar-outline' },
     to: 'calendar',
+    permissions: ['auth.*'],
   },
 ] as VerticalNavItems
