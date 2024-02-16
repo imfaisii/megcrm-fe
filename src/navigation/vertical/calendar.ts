7f52d483121f0e6729b3bcedd3c2e490a8b8ff3e
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2024-02-16 20:26:52 +0000

    (fix): lead-generator

--- src/navigation/vertical/calendar.ts
+++ src/navigation/vertical/calendar.ts
@@ -3,12 +3,10 @@ import type { VerticalNavItems } from '@/@layouts/types'
 export default [
   {
     heading: 'Events',
-    permissions: ['auth.*']
   },
   {
     title: 'Calendar',
     icon: { icon: 'mdi-calendar-outline' },
     to: 'calendar',
-    permissions: ['auth.*'],
   },
 ] as VerticalNavItems
--- src/pages/leads/index.vue
+++ src/pages/leads/index.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup>
 import LeadsTable from "@/components/leads/LeadsTable.vue";
+import { modules } from "@/constants/modules";
+import { usePermissionsStore } from "@/stores/permissions/usePermissionsStore";
 
 const isAddLeadDialogVisible: any = ref(false);
+const store = usePermissionsStore();
 </script>
 <template>
   <section>
@@ -18,6 +21,7 @@ const isAddLeadDialogVisible: any = ref(false);
                 <h6 class="text-h5">Leads Table</h6>
               </VCol>
               <VCol
+                v-if="store.can([modules.LEADS + '.create'])"
                 cols="12"
                 lg="6"
                 :class="$vuetify.display.lgAndUp ? 'text-right' : 'text-center'"
