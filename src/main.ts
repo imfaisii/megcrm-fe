4414a88ece387d0d5e16a29b4228cc7db14082bf
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2024-02-21 13:47:23 +0000

    (fix): lazyload

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,19 @@ import vuetify from '@/plugins/vuetify'
 import { loadFonts } from '@/plugins/webfontloader'
 import router from '@/router'
 import '@core/scss/template/index.scss'
+import errorimage from "@images/custom/404.jpg"
+import loadimage from "@images/custom/loading.gif"
 import '@styles/styles.scss'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import VueEasyLightbox from 'vue-easy-lightbox'
+import VueLazyload from 'vue-lazyload'
 import ToastPlugin from 'vue-toast-notification'
 
 // import './style.css'
 import 'primevue/resources/themes/aura-light-green/theme.css'
 import 'vue-easy-lightbox/dist/external-css/vue-easy-lightbox.css'
-
 
 loadFonts()
 
@@ -25,6 +27,12 @@ const app = createApp(App)
 
 // Use plugins
 app.use(createPinia())
+app.use(VueLazyload, {
+  preLoad: 1.3,
+  error: errorimage,
+  loading: loadimage,
+  attempt: 1
+})
 app.use(vuetify)
 app.use(router)
 app.use(PrimeVue)
