e4d1238b31be3cf51d3d29579ea3c7008c211e55
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2024-02-23 17:43:31 +0000

    (fix): ui

--- src/components/leads/details/AirCallHistory.vue
+++ src/components/leads/details/AirCallHistory.vue
@@ -1,194 +1,177 @@
-<script setup lang="js">
+<script lang="ts" setup>
+import useApiFetch from "@/composables/useApiFetch";
 import useTime from "@/composables/useTime";
+import { useToast } from "@/plugins/toastr";
 import { useLeadsStore } from "@/stores/leads/useLeadsStore";
-import useApiFetch from '@/composables/useApiFetch'
-import { useToast } from '@/plugins/toastr'
-import {ref} from 'vue';
-import {
-  requiredValidator,
-} from "@validators";
-
+import { requiredValidator } from "@validators";
+import { ref } from "vue";
 
 const leadStore = useLeadsStore();
 const time = useTime();
-const isDialogVisible = ref(false);
-const Loader = ref(false);
+const loader = ref(false);
 const toast = useToast();
-const errorMessage = ref('');
-const AirCallFormRef = ref();
-const form= ref({
-  'phone_number':null
+const errorMessage = ref("");
+const airCallFormRef = ref();
+const responseData = ref([]);
+const form: any = ref({
+  phone_number: null,
 });
-const oldNumberSearched = ref(null);
-onMounted(() => {
-  form.value.phone_number = leadStore?.selectedLead?.phone_no;
-})
-const isInputSame = computed(() => form.value.phone_number === oldNumberSearched.value);
-const responseData =  ref(null);
 
+const fixNumber = (str: string): string => str.replace(/\D/g, "").slice(-10);
 
-const AircallSerach = async () => {
-  try{
-  const {valid:valid} = await  AirCallFormRef?.value.validate();
-    if(!valid)
-    return ;
-      Loader.value = true;
-      ({data:responseData.value} = await useApiFetch("/aircall/search-call",{
-        data: form.value,
-        method: 'POST',
-      }));
-      errorMessage.value = '';
-      oldNumberSearched.value = form.value.phone_number;
-  }catch(err){
-    if (err.response.status === 422) {
-      errorMessage.value = err?.response?.data?.errors?.phone_number?.[0]
+const getCallData = async (number: any) => {
+  try {
+    loader.value = true;
+
+    const { data } = await useApiFetch("/aircall/search-call", {
+      data: {
+        phone_number: "+44" + number.phone_number,
+      },
+      method: "POST",
+    });
+
+    responseData.value = data;
+  } catch (e: any) {
+  } finally {
+    loader.value = false;
+  }
+};
+
+const airCallSearch = async () => {
+  try {
+    const { valid: valid } = await airCallFormRef?.value.validate();
+    if (!valid) return;
+
+    getCallData(form.value);
+
+    errorMessage.value = "";
+  } catch (err: any) {
+    if (err?.response?.status === 422) {
+      errorMessage.value = err?.response?.data?.errors?.phone_number?.[0];
     } else {
-      toast.error(err);
+      toast.error(err.message);
     }
   }
-  finally{
-    Loader.value = false;
-  }
-
-
 };
+
+onMounted(() => {
+  form.value.phone_number = fixNumber(leadStore?.selectedLead?.phone_no);
+
+  if (form.value.phone_number.length === 10) {
+    getCallData(form.value);
+  } else {
+    toast.error("Invalid number, please update and click search button.");
+  }
+});
 </script>
 
 <template>
-  <VCard title="Call History" class="pa-5">
+  <VCard title="Calls History ( AIR CALL )">
+    <template #append>
+      <div class="me-n3 mt-n2">
+        <VCol cols="12">
+          <VForm ref="airCallFormRef" @submit.prevent="airCallSearch">
+            <div
+              class="app-user-search-filter d-flex flex-wrap align-center gap-x-6 gap-y-4"
+            >
+              <VTextField
+                v-model="form.phone_number"
+                :rules="[requiredValidator]"
+                label="Phone Number"
+                placeholder="7943111370"
+                density="compact"
+                style="min-inline-size: 14rem"
+                prefix="+44"
+                class="order-2 order-sm-1"
+                clearable
+                hide-details
+                single-line
+                required
+                :maxlength="10"
+                :minLength="10"
+                :error-messages="errorMessage"
+              />
+
+              <VBtn
+                class="order-sm-2 order-1"
+                size="small"
+                type="submit"
+                :disabled="loader"
+                :loading="loader"
+                icon="mdi-magnify"
+              />
+            </div>
+          </VForm>
+        </VCol>
+      </div>
+    </template>
+
     <VCardText>
-      <VList
-        lines="two"
-        class="air-call-history-list card-list custom-overflow"
-      >
-        <template #default>
-          <VAlert class="mb-4" border="start" color="info" variant="tonal">
-            Enter an
-            <v-icon>
-              <v-tooltip activator="parent" location="start">
-                <span
-                  >164 numbers are formatted [+] [country code] [subscriber
-                  number including area code] and can have a maximum of fifteen
-                  digits</span
+      <VAlert class="mb-6" border="start" color="info" variant="tonal">
+        Enter an e.164 format number to get the history.
+      </VAlert>
+
+      <VRow>
+        <VCol cols="12">
+          <VTable class="mt-4" fixed-header>
+            <thead>
+              <tr>
+                <th class="text-uppercase">Caller Name</th>
+                <th class="text-uppercase">Call Start Time</th>
+                <th class="text-uppercase">Call End Time</th>
+                <th class="text-uppercase">duration</th>
+                <th class="text-uppercase">Phone Number</th>
+                <th class="text-uppercase">Direction</th>
+              </tr>
+            </thead>
+            <tbody class="text-high-emphasis">
+              <tr v-if="responseData.length < 1">
+                <td
+                  class="font-italic font-weight-medium text-center"
+                  colspan="6"
                 >
-              </v-tooltip>
-              mdi-information-outline
-            </v-icon>
-            E.164 Format Number to get the history
-          </VAlert>
-
-          <section>
-            <VRow>
-              <VCol cols="12">
-                <VCard class="air-call-search-card">
-                  <VRow class="pa-4">
-                    <VCol cols="12">
-                      <v-form
-                        @submit.prevent="AircallSerach"
-                        ref="AirCallFormRef"
-                      >
-                        <VRow>
-                          <VCol cols="12" lg="8">
-                            <VTextField
-                              v-model="form.phone_number"
-                              :rules="[requiredValidator]"
-                              label="Phone Number"
-                              placeholder="+441514960453"
-                              :counter="15"
-                              clearable
-                              required
-                              :error-messages="errorMessage"
-                            />
-                          </VCol>
-
-                          <VCol cols="4" lg="4">
-                            <!-- Search button -->
-                            <VBtn
-                              class="float-right"
-                              type="submit"
-                              :disabled="
-                                Loader || !form.phone_number || isInputSame
-                              "
-                              :loading="Loader"
-                            >
-                              Search
-                            </VBtn>
-                          </VCol>
-                        </VRow>
-                      </v-form>
-                    </VCol>
-                  </VRow>
-                  <VTable class="mt-4" fixed-header v-show="!Loader">
-                    <thead>
-                      <tr>
-                        <th class="text-uppercase">Caller Name</th>
-                        <th class="text-uppercase">Call Start Time</th>
-                        <th class="text-uppercase">Call End Time</th>
-                        <th class="text-uppercase">duration</th>
-                        <th class="text-uppercase">Phone Number</th>
-                        <th class="text-uppercase">Direction</th>
-                      </tr>
-                    </thead>
-                    <tbody class="text-high-emphasis">
-                      <tr v-if="!responseData">
-                        <td
-                          class="font-italic font-weight-medium text-center"
-                          colspan="6"
-                        >
-                          No records found.
-                        </td>
-                      </tr>
-                      <tr v-else v-for="(callRecord, index) in responseData">
-                        <td>
-                          <p class="font-weight-bold">
-                            {{ callRecord?.user?.name }}
-                          </p>
-                        </td>
-                        <td>
-                          <p class="font-weight-medium">
-                            {{
-                              time.convertTimeStampToDate(
-                                callRecord?.started_at
-                              )
-                            }}
-                          </p>
-                        </td>
-                        <td>
-                          <p class="font-weight-medium">
-                            {{
-                              time.convertTimeStampToDate(callRecord?.ended_at)
-                            }}
-                          </p>
-                        </td>
-                        <td>
-                          <p class="font-weight-medium">
-                            {{
-                              time.convertSecondsToMinutes(
-                                callRecord?.duration ?? 0
-                              )
-                            }}
-                            mins
-                          </p>
-                        </td>
-                        <td>
-                          <p class="text-button font-italic">
-                            {{ callRecord?.number?.digits }}
-                          </p>
-                        </td>
-                        <td>
-                          <p class="text-button font-italic">
-                            {{ callRecord?.direction }}
-                          </p>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </VTable>
-                </VCard>
-              </VCol>
-            </VRow>
-          </section>
-        </template>
-      </VList>
+                  No records found.
+                </td>
+              </tr>
+              <tr v-else v-for="(callRecord, index) in (responseData as any)">
+                <td>
+                  <p class="font-weight-bold">
+                    {{ callRecord?.user?.name }}
+                  </p>
+                </td>
+                <td>
+                  <p class="font-weight-medium">
+                    {{ time.convertTimeStampToDate(callRecord?.started_at) }}
+                  </p>
+                </td>
+                <td>
+                  <p class="font-weight-medium">
+                    {{ time.convertTimeStampToDate(callRecord?.ended_at) }}
+                  </p>
+                </td>
+                <td>
+                  <p class="font-weight-medium">
+                    {{
+                      time.convertSecondsToMinutes(callRecord?.duration ?? 0)
+                    }}
+                    mins
+                  </p>
+                </td>
+                <td>
+                  <p class="text-button font-italic">
+                    {{ callRecord?.number?.digits }}
+                  </p>
+                </td>
+                <td>
+                  <p class="text-button font-italic">
+                    {{ callRecord?.direction }}
+                  </p>
+                </td>
+              </tr>
+            </tbody>
+          </VTable>
+        </VCol>
+      </VRow>
     </VCardText>
   </VCard>
 </template>
@@ -197,11 +180,13 @@ const AircallSerach = async () => {
 .card-list {
   --v-card-list-gap: 1.5625rem;
 }
+
 .custom-overflow {
   overflow: visible !important;
 }
+
 .float-right {
-  float: right;
+  float: inline-end;
 }
 </style>
 
