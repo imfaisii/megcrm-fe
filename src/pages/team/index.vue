2619dc276d8b798ba313a8313b01ce28275b7eee
Haamzaaay <85665625+Haamzaaay@users.noreply.github.com>
2024-02-23 17:02:58 +0000

    team component

---
+++ src/pages/team/index.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import UsersTable from "@/components/users/UsersTable.vue";
+import { useUsersStore } from "@/stores/users/useUsersStore";
+
+const store = useUsersStore();
+const isAddUserDialogVisible: any = ref(false);
+</script>
+<template>
+  <section>
+    <VRow>
+      <VCol cols="12">
+        <VCard class="users-card">
+          <VCardTitle class="pl-4 pr-4 mt-3 mb-2">
+            <VRow class="text-layout">
+              <VCol
+                cols="12"
+                lg="6"
+                :class="$vuetify.display.lgAndUp ? 'text-left' : 'text-center'"
+              >
+                <h6 class="text-h5">Teams Table</h6>
+              </VCol>
+              <VCol
+                cols="12"
+                lg="6"
+                :class="$vuetify.display.lgAndUp ? 'text-right' : 'text-center'"
+              >
+                <VBtn @click="isAddUserDialogVisible = true" color="primary">
+                  Create User
+                </VBtn>
+              </VCol>
+            </VRow>
+          </VCardTitle>
+
+          <UsersTable />
+        </VCard>
+      </VCol>
+    </VRow>
+    <AddEditUserDialog
+      v-model:is-dialog-visible="isAddUserDialogVisible"
+      :store="store"
+    />
+  </section>
+</template>
---
+++ src/stores/team/useTeamStore.ts
@@ -0,0 +1,162 @@
+import useApiFetch from '@/composables/useApiFetch'
+import { defaultPagination } from '@/constants/pagination'
+import { useToast } from '@/plugins/toastr'
+import { EventBus } from '@/utils/useEventBus'
+import { handleError, reshapeParams } from '@/utils/useHelper'
+import { defineStore } from 'pinia'
+
+type UserData = {
+  name: string
+}
+
+export const defaultModel = {
+  id: null,
+  name: null,
+}
+
+export const useUsersStore = defineStore('users', () => {
+  const endPoint = '/team'
+  const entity = 'Team'
+  const users = ref<any>([])
+  const selected = ref<any>(defaultModel)
+  const selectedId = ref<any>(null)
+  const errors = ref<any>({})
+  const isLoading = ref<any>(false)
+  const meta = ref<any>(defaultPagination)
+  const $toast: any = useToast()
+  const includes = ["createdBy"]
+
+  const isSelected = computed(() => !!selected.value.id)
+
+  const index = async (options = {}) => {
+    isLoading.value = true
+    const { data, meta: serverMeta } = await useApiFetch(reshapeParams(endPoint, meta.value, options))
+    users.value = data.users
+    meta.value = {
+      filters: meta.value?.filters ?? {},
+      ...serverMeta
+    }
+    isLoading.value = false
+  }
+
+  const get = async (userId: Number) => {
+    isLoading.value = true
+    selectedId.value = userId
+    const { data } = await useApiFetch(`${endPoint}/${userId}`)
+    selected.value = data.user
+    selected.value.roles = data.user.roles.map((i: any) => i.id)
+    isLoading.value = false
+    EventBus.$emit('toggle-users-dialog', true)
+  }
+
+  const store = async (userData: UserData, options: any = { method: 'POST' }) => {
+    try {
+      isLoading.value = true
+      await useApiFetch('/users', {
+        data: userData,
+        ...options,
+      })
+      $toast.success('User was saved successfully.')
+      errors.value = {}
+      EventBus.$emit('toggle-users-dialog', false)
+      await index()
+    } catch (error) {
+      handleError(error, errors)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  const destroy = async (id: number, options = { method: 'DELETE' }) => {
+    try {
+      isLoading.value = true
+      selectedId.value = id
+      await useApiFetch(`${endPoint}/${id}`, options)
+      $toast.success(`${entity} was deleted successfully.`)
+      await index()
+    } catch (error) {
+      handleError(error, errors)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  const update = async (id: number | string, payload: any, options = { method: 'PUT' }) => {
+    try {
+      isLoading.value = true
+      await useApiFetch(`${endPoint}/${id}`, {
+        data: payload,
+        ...options
+      })
+      $toast.success(`${entity} was updated successfully.`)
+      errors.value = {}
+      EventBus.$emit('toggle-users-dialog', false)
+      await index()
+    } catch (error) {
+      handleError(error, errors)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  const updateProfile = async (id: number | string, payload: any, options = { method: 'PUT' }) => {
+    try {
+      errors.value = {}
+      isLoading.value = true
+      await useApiFetch(`${endPoint}/${id}/profile`, {
+        data: payload,
+        ...options
+      })
+      $toast.success(`Profile was updated successfully.`)
+      errors.value = {}
+    } catch (error) {
+      handleError(error, errors)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  const reset = () => {
+    selected.value = { ...defaultModel }
+    errors.value = {}
+  }
+
+  const resolveUserRoleVariant = (role: string) => {
+    const roleLowerCase = role.toLowerCase();
+
+    if (roleLowerCase === "super admin")
+      return { color: "warning", icon: "tabler-device-laptop" };
+
+    return { color: "primary", icon: "tabler-user" };
+  };
+
+  const resolveUserStatusVariant = (stat: string) => {
+    if (stat) {
+      return "success";
+    }
+
+    return "error";
+  };
+
+
+  return {
+    users,
+    selectedId,
+    isLoading,
+    isSelected,
+    selected,
+    meta,
+    errors,
+    includes,
+
+    resolveUserStatusVariant,
+    resolveUserRoleVariant,
+    reset,
+    index,
+    get,
+    store,
+    destroy,
+    update,
+    updateProfile
+  }
+})
